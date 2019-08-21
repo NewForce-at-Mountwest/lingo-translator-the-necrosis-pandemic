7edebf435aad15d5e691f8ee86183fa753fed0ed
Calebooski <mrmeadows2018@gmail.com>
2019-08-21 15:46:48 -0400

    Progress on french js

--- scripts/french.js
+++ scripts/french.js
@@ -47,6 +47,60 @@ const frenchData = {
   }
 };
 
+const fNav = document.querySelector("#french");
+console.log(fNav);
+
+const fContent = document.querySelector("#language-container")
+
+//Function for people
+function fPeopleFunction (){
+  const fPeopleEmptyString = ""
+  fContent.innerHTML += `<h1> Notable People </h1>`
+  for (i = 0; i < frenchData.notablePeople.length; i++)
+  var fPeople = `<li> ${frenchData.notablePeople[i]} </li>`
+ console.log(fPeople) 
+ fContent.innerHTML += `<ul>${fPeople}</ul>`
+};
+
+//Function for countries
+  function fCountriesSpokenFunction (){
+    const fCountriesEmptyString = ""
+      fContent.innerHTML += `<h2> Countries Spoken </h2>`
+      for (i = 0; i < frenchData.countriesSpoken.length; i++){
+    var fCountries = `<li> ${frenchData.countriesSpoken[i]} </li>`
+    console.log(fCountries)
+    fContent.innerHTML += `<ul>${fCountries}</ul>`
+  }
+ };
+
+ //Function for Funfacts
+ function fFunFactsFunction (){
+   let fFactsHTMLString = ""
+  fContent.innerHTML += `<h2> Fun Facts </h2>`
+  for (i = 0; i < frenchData.funFacts.relatedLanguages.length; i++){
+    var fLanguages = `<li> ${frenchData.funFacts.relatedLanguages[i]} </li>`
+    console.log(fLanguages)
+    fContent.innerHTML += `<ul>${fLanguages}</ul>`
+    }
+      fContent.innerHTML += `<h4 class="alphabetAmt">Letters in alphabet</h4> <p> ${frenchData.funFacts.lettersInAlphabet}</p>`
+      fContent.innerHTML += `<h4 class="peopleSpeaking">Amount of speakers</h4> <p> ${frenchData.funFacts.numberOfSpeakers}</p>`
+ };
+
+function fDictionaryExamples (){
+  fContent.innerHTML += `<h2> Dictionary </h2>`
+  for (i = 0; i < frenchDate.dictionary.length; i++){
+
+  }
+}
+// Call the functions
+fNav.addEventListener("click", function() {
+
+  fPeopleFunction ();
+  fCountriesSpokenFunction ();
+  fFunFactsFunction ();
+})
+
+
 
 
 
